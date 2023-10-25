c2a53cd10e012cad4dc65dbce1f48c8cee015a84
Allison Sulca <asulca2112@gmail.com>
2023-10-25 17:26:34 -0500

    se ajustaron estilos mobile

--- src/views/home.js
+++ src/views/home.js
@@ -2,17 +2,24 @@
 function home(navigateTo) {
     const section = document.createElement('section');
     section.classList.add('sectionho');
-    const title = document.createElement('h2');
     section.classList.add('textho');
+    const title = document.createElement('h2');
+    // section.classList.add('textho');
     const buttonLogin = document.createElement('button');
     buttonLogin.classList.add('buttonho');
     const buttonRegister = document.createElement('button');
     buttonRegister.classList.add('buttonho');
     const image = document.createElement('img');
     image.classList.add('imgho');
+    const buttonGoogle = document.createElement('button');
+    buttonGoogle.classList.add('googlelog');
+    const other = document.createElement('h6');
+    other.classList.add('otherOption');
   
     image.src = './img-sn/festilogo1.png';
     title.textContent = 'Welcome to Festigram, the largest community of concerts and festivals around the world!';
+    other.textContent = 'You can access with you Google Account';
+    buttonGoogle.textContent = 'Google';
    
     buttonLogin.textContent = 'Login';
     buttonLogin.addEventListener('click', () => {
@@ -25,7 +32,7 @@ function home(navigateTo) {
     });
     
   
-    section.append(image, title, buttonLogin, buttonRegister);
+    section.append(image, title, buttonLogin, buttonRegister, other, buttonGoogle);
     return section;
   }
   
